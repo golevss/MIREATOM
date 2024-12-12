0d0db18c492db111c71e71a990aeaf9a3908a8cd
LondonDungeon <dokoeboi@gmail.com>
2024-12-12 03:15:53 +0300

    Big fix with greek letter

--- lexer.py
+++ lexer.py
@@ -1,181 +1,203 @@
-import numpy as np
 import time
 
+var_dict = {}
+clear_var_dict = {}
+const_dict = {}
+clear_const_dict = {}
+sep_words = ['+','-','*','/','^','(',')','{','}','[',']','\\','_',' ','&',',']
+greek_words = ['\\alpha','\\beta','\\Gamma','\\gamma','\\Delta','\\delta','\\epsilon','\\varepsilon','\\zeta','\\eta'
+    ,'\\Theta','\\theta','\\vartheta','\\iota','\\kappa','\\varkappa','\\Lambda','\\lambda','\\mu','\\nu','\\Xi','\\xi'
+    ,'\\Pi','\\pi','\\varpi','\\rho','\\varrho','\\Sigma','\\sigma','\\varsigma','\\tau','\\Upsilon','\\upsilon','\\Phi'
+    ,'\\phi','\\varphi','\\chi','\\Psi','\\psi','\\Omega','\\omega']
+
+# Обезличивание греческих букв
+def proc_greek_str(string):
+    for word in greek_words:
+        idx = string.find(word)
+        while idx != -1:
+            string = string[:idx] + '#' + string[idx + len(word):]
+            var_dict[idx] = word
+            idx = string.find(word, idx + 1)
+
+    return string
+
+# Обезличивание переменных и констант
 def proc_str(string):
-    # Обезличивание переменных
-    sep_words = ['+','-','*','/','^','(',')','{','}','\\','_',' ','&',',']
-    f_string = ""
+    string += ' '    
+    proc_string = ""
     i = 0
+    # Замена переменных и констант на # и @ соответственно
     while (i != len(string)):
         if string[i] == "\\":
-            f_string += string[i]
+            proc_string += string[i]
             i += 1
             while (not(string[i] in sep_words)):
-                f_string += string[i]
+                proc_string += string[i]
                 i += 1
             i -= 1
-        elif (string[i] in sep_words or string[i].isdigit() or string[i] == 'f' or string[i] == 'd'):
-            f_string +=string[i]
+        elif (string[i] in sep_words or string[i] == 'f' or string[i] == 'd'):
+            proc_string +=string[i]
         elif (string[i].isalpha()):
-            f_string += '#'
+            proc_string += '#'
+        elif (string[i].isdigit()):
+            proc_string += '@'
         else:
-            f_string +=string[i]
+            proc_string +=string[i]
         i += 1
+    # Замена греческих букв
+    proc_string = proc_greek_str(proc_string).strip()
+    # Удаление повторяющихся @
+    result = proc_string[0]
+    for char in proc_string[1:]:
+        if char != '@' or result[-1] != '@':
+            result += char
 
-    return f_string
+    return result
 
-def jaccpp(str1, str2):
-    # Расширенный коэффициент Жаккара
-    set1 = set(str1)
-    set2 = set(str2)
-    intersection = len(set1.intersection(set2))
-    union = len(set1) + len(set2) - intersection
-    return (intersection / union) * 100
-
-def sert(str1, str2):
-    # Дистанция Сёрта
-    bigrams1 = {str1[i:i+2] for i in range(len(str1)-1)}
-    bigrams2 = {str2[i:i+2] for i in range(len(str2)-1)}
-    intersection = bigrams1.intersection(bigrams2)
-    return (2 * len(intersection) / (len(bigrams1) + len(bigrams2))) * 100
-
-def leven(str1, str2):
-    # Расстояние Левенштейна 
-    len_str1, len_str2 = len(str1), len(str2)
-    matrix = np.zeros((len_str1 + 1, len_str2 + 1))
-
-    for i in range(len_str1 + 1):
-        matrix[i][0] = i
-    for j in range(len_str2 + 1):
-        matrix[0][j] = j
-    for i in range(1, len_str1 + 1):
-        for j in range(1, len_str2 + 1):
-            cost = 0 if str1[i - 1] == str2[j - 1] else 1
-            matrix[i][j] = min(matrix[i - 1][j] + 1,
-                               matrix[i][j - 1] + 1,
-                               matrix[i - 1][j - 1] + cost)
-
-    lev_distance = matrix[len_str1][len_str2]
-    max_len = max(len_str1, len_str2)
-    return (1 - lev_distance / max_len) * 100
-
-def coolPin(str1,str2):
-    # Коэффициент Кульпина
-    str1 = str1.replace('\f','\\f')
-    str1 = str1.replace('\b','\\b')
-    str1 = str1.replace('\t','\\t')
-    str1 = str1.replace('\n','\\n')
-    str1 = str1.replace('\r','\\r')
-
-    str2 = str2.replace('\f','\\f')
-    str2 = str2.replace('\b','\\b')
-    str2 = str2.replace('\t','\\t')
-    str2 = str2.replace('\n','\\n')
-    str2 = str2.replace('\r','\\r')
-    
-    str1 = str1.replace('\\frac','')
-    str2 = str2.replace('\\frac','')
-    str1 = proc_str(str1)
-    str2 = proc_str(str2)
-
-    return ((leven(str1,str2) + jaccpp(str1,str2) + sert(str1,str2)) / 3)
-
+# Нахождение общих подстрок и их фильтрация 
 def find_parts(str1, str2):
-    # Нахождение общих подстрок и их фильтрация 
     common_substrings = []
-
+    # Нахождение всех схожих подстрок
     for i in range(len(str1)):
         for j in range(i + 1, len(str1) + 1):
             if str1[i:j] in str2:
                 common_substrings.append(str1[i:j])
-    all_values = sorted(list(set(common_substrings)))
+    all_templates = sorted(list(set(common_substrings)))
     
-    values = []
+    templates = []
     max_len = -1
-    for i in range (len(all_values)):
-        if (len(all_values[i]) > max_len):
-            max_len = len(all_values[i])
-        elif (len(all_values[i]) <= max_len):
-            values.append(all_values[i - 1])
-            max_len = len(all_values[i])
-    if len(all_values) != 0:
-        values.append(all_values[-1])
+    # Сортировка подстрок по длинне
+    for i in range (len(all_templates)):
+        if (len(all_templates[i]) > max_len):
+            max_len = len(all_templates[i])
+        elif (len(all_templates[i]) <= max_len):
+            templates.append(all_templates[i - 1])
+            max_len = len(all_templates[i])
+    if len(all_templates) != 0:
+        templates.append(all_templates[-1])
 
-    values = sorted(values,key = len, reverse = True)
-    rm_cont = []
-    for el1 in values:
-        for el2 in values:
-            if el1 != el2 and el2 in el1 or '#' not in el2:
-                rm_cont.append(el2)
-    for el1 in rm_cont:
-        for el2 in values:
+    key_words = ['#','@']
+    templates = sorted(templates,key = len, reverse = True)
+    rm_temp = []
+    # Нахождение неуникальных строк
+    for el1 in templates:
+        for el2 in templates:
+            if (el1 != el2 and el2 in el1) or all(word not in el2 for word in key_words):
+                rm_temp.append(el2)
+    # Удаление неуникальных строк
+    for el1 in rm_temp:
+        for el2 in templates:
             if el2 == el1:
-                values.remove(el2)
+                templates.remove(el2)
 
-    return values
+    return templates
 
-def replace_chars(base_string, pattern):
-    # Взовращение переменным имени
-    base_len = len(base_string)
-    pattern_len = len(pattern)
+# Формирование словарей для подстановки переменных и констант
+def make_dicts(string,str_temp):
+    var_dict.clear()
+    const_dict.clear()
+    clear_var_dict.clear()
+    clear_const_dict.clear()
     
-    for i in range(base_len - pattern_len + 1):
-        candidate = base_string[i:i + pattern_len]
-        
-        if all(c1 == c2 or c2 == '#' for c1, c2 in zip(candidate, pattern)):
-            result = ''.join(c1 if c2 == '#' else c2 for c1, c2 in zip(candidate, pattern))
-            return result
-    return pattern
+    string += ' '    
+    i = 0
+    # Нахождение индексов всех переменных и констант
+    while (i != len(string)):
+        if string[i] == "\\":
+            i += 1
+            while (not(string[i] in sep_words)):
+                i += 1
+            i -= 1
+        elif (string[i] in sep_words or string[i] == 'f' or string[i] == 'd'):
+            i = i
+        elif (string[i].isalpha()):
+            var_dict[i] = string[i]
+        elif (string[i].isdigit()):
+            const_dict[i] = string[i]
+        else:
+            i = i
+        i += 1
+    # Обработка греческих символов
+    proc_greek_str(string)
+    # Объединение констант в одно число, если они им являются 
+    for key in sorted(const_dict, reverse=True):
+        if key - 1 in const_dict:
+            const_dict[key - 1] = const_dict[key - 1] + const_dict[key]
+            del const_dict[key]
+    # Корректировка словоря констант по индексам
+    list_temp = list(str_temp)
+    for key in sorted(const_dict):
+        for i in range (len(str_temp)):
+            if list_temp[i] == '@':
+                clear_const_dict[i] = const_dict[key]
+                list_temp[i] = ''
+                break
+    # Корректировка словоря переменных по индексам
+    for key in sorted(var_dict):
+        for i in range (len(str_temp)):
+            if list_temp[i] == '#':
+                clear_var_dict[i] = var_dict[key]
+                list_temp[i] = ''
+                break
+    
+# Взовращение переменным и константам имена
+def replace_chars(template,str_temp):
+    list_temp = list(template)
+    temp_idx = str_temp.find(template)
+    temp_size = temp_idx + len(list_temp)
+    # Работа с переменными
+    for idx,var in clear_var_dict.items():
+        if idx >= temp_idx and idx < temp_size:
+            list_temp[idx - temp_idx] = var
+    # Работа с константами
+    for idx,const in clear_const_dict.items():
+        if idx >= temp_idx and idx < temp_size:
+            list_temp[idx - temp_idx] = const
+    
+    return ''.join(list_temp)
 
-def fin_parts(s,v):
-    # Массив с переменными в строку
-    for i in range (len(v)):
-        v[i] = replace_chars(s,v[i]).strip()
-    return '$'.join(map(str, v))
-
+# Массив с переменными в строку
+def fin_parts(string,str_temp,templates):
+    make_dicts(string,str_temp)
+    for i in range (len(templates)):
+        templates[i] = replace_chars(templates[i].strip(),str_temp)
+    return '$'.join(templates)
+    
+# Нахождение общих частей
 def sparts(string1,string2):
+    replacements = {'\a' : '\\a','\b' : '\\b','\c' : '\\c','\d' : '\\d','\e' : '\\e','\f' : '\\f','\g' : '\\g'
+    ,'\h' : '\\h','\i' : '\\i','\j' : '\\j','\k' : '\\k','\l' : '\\l','\m' : '\\m','\n' : '\\n','\p' : '\\p'
+    ,'\o' : '\\o','\q' : '\\q','\r' : '\\r','\s' : '\\s','\t' : '\\t','\v' : '\\v','\w' : '\\w'
+    ,'\y' : '\\y'}
+    # Корректировка строк
+    for old, new in replacements.items():
+        string1 = string1.replace(old, new)
+        string2 = string2.replace(old, new)
+    # Обработка строк обезличиванием переменных
+    str1, str2 = proc_str(string1).strip(),proc_str(string2).strip()
     # Нахождение общих частей
-    string1 = string1.replace('\f','\\f')
-    string1 = string1.replace('\b','\\b')
-    string1 = string1.replace('\t','\\t')
-    string1 = string1.replace('\n','\\n')
-    string1 = string1.replace('\r','\\r')
-
-    string2 = string2.replace('\f','\\f')
-    string2 = string2.replace('\b','\\b')
-    string2 = string2.replace('\t','\\t')
-    string2 = string2.replace('\n','\\n')
-    string2 = string2.replace('\r','\\r')
-    str1 = proc_str(string1)
-    str2 = proc_str(string2)
-    values = find_parts(str1 ,str2)
-    values_c = values.copy()
-
-    fstring = fin_parts(string1,values)
-    sstring = fin_parts(string2,values_c)
-    koef = (((len(fstring.replace("$","")) / len(string1)) *100 + (len(sstring.replace("$","")) / len(string2)) *100) / 2)
-    # return str(int(koef)) + '@' + fstring + '@' + sstring
-    return str(int(koef))
+    templates = find_parts(str1 ,str2)
+    templates_c = templates.copy()
+    # Подстановка переменных и констант в схожие части
+    same_parts_string1, same_parts_string2 = fin_parts(string1,str1,templates), fin_parts(string2,str2,templates_c)
+    # Вычисление коэфициента схожести
+    koef = (((len(same_parts_string1.replace("$","")) / len(string1)) *100 + (len(same_parts_string2.replace("$","")) / len(string2)) *100) / 2)
+    return str(int(koef)) + '\n\n' + same_parts_string1.replace("$","\n") + '\n\n' + same_parts_string2.replace("$","\n")
+    # return str(int(koef))
 
 
 def main(in_string1,in_string2):
     start_time = time.time()
-    a = coolPin(in_string1,in_string2)
-    end_time = time.time()
-    print("\nВремя коэфа Кульпина:",end_time - start_time )
-    start_time = time.time()
     b = sparts(in_string1,in_string2)
     end_time = time.time()
     print("Время коэфа Ёлки:",end_time - start_time )
-    print(f"Кульпин: {int(a)}")
     print(f"Ёлка: {b}\n")
      
     
 
 if __name__ == "__main__":
     # Место для формул
-    latex1 = "\frac{\sin(x+2)}{x + 51} * 5a + \log_a{xy + 2} + \log{x + 2} + \sqrt{a}"
-    latex2 = "\log{a + 2} + \sqrt{x} + \log_x{ab + 2} + 5c + \frac{\sin(a+2)}{a + 51}"
-
+    latex1 = "\alpha + \beta = \gamma"
+    latex2 = "\alpha + \beta + \delta = \gamma"
     main(latex1,latex2)
     
